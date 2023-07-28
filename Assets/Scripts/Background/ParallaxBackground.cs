78b1aa06b388583d906e763b5ee82aca764d6ce3
Май Ван Туан <8268826@gmail.com>
2023-07-28 14:22:33 +0700

    Parallax background

---
+++ Assets/Scripts/Background/ParallaxBackground.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+namespace Background
+{
+    public class ParallaxBackground : MonoBehaviour
+    {
+        private Camera _camera;
+        private float _xPosition;
+        [SerializeField] private float _parallaxEffect;
+
+        private void Start()
+        {
+            _camera = Camera.main;
+            _xPosition = transform.position.x;
+        } 
+
+        private void Update()
+        {
+            var distanceToMove = _camera.transform.position.x * _parallaxEffect;
+            transform.position = new Vector3(_xPosition + distanceToMove, transform.position.y);
+        }
+    }
+} 
