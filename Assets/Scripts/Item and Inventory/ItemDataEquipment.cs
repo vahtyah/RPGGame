9b4437d6411d34cd16ff410e52de108deca1c4a5
Май Ван Туан <8268826@gmail.com>
2023-08-04 16:37:30 +0700

    Equipment Item

---
+++ Assets/Scripts/Item and Inventory/ItemDataEquipment.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Item_and_Inventory
+{
+    public enum EquipmentType
+    {
+        Weapon,
+        Armor,
+        Amulet,
+        Flask
+    }
+    
+    [CreateAssetMenu(fileName = "New Item Data Equipment", menuName = "Data/Item Equipment")]
+    public class ItemDataEquipment : ItemData
+    {
+        public EquipmentType equipmentType;
+    }
+}
