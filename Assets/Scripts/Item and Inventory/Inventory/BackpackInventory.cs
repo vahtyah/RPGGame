657a7cbe86897b616c31e43e5b98e6d879413f5f
Май Ван Туан <8268826@gmail.com>
2023-09-09 22:58:21 +0300

    fix save inventory

--- Assets/Scripts/Item and Inventory/Inventory/BackpackInventory.cs
+++ Assets/Scripts/Item and Inventory/Inventory/BackpackInventory.cs
@@ -36,7 +36,7 @@ namespace Item_and_Inventory.Test
         {
             base.LoadData(data);
             foreach (var item in data.generalInventory)
-                loadedItems.Add(item);
+                loadedItems.Add(item.Key);
         }
 
         public override void SaveData(ref GameData data)
@@ -44,7 +44,7 @@ namespace Item_and_Inventory.Test
             data.generalInventory.Clear();
             base.SaveData(ref data); 
             foreach (var value in itemDictionary)
-                data.generalInventory.Add(value.Value);
+                data.generalInventory.Add(value.Value, 1);
         }
     }
 }
